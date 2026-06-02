36a542a92c3b4e7457fb7404194c95c59fe66edf
arpitgoswami03 <arpit.personal.2003@gmail.com>
2026-06-02 07:45:42 +0530

    feat: api creation for in-memory crud operations

---
+++ 01-api-creation-for-crud/main.py
@@ -0,0 +1,96 @@
+from fastapi.datastructures import Default
+from fastapi import FastAPI
+from pydantic import BaseModel
+import copy
+from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+
+
+class RequestSample(BaseModel):
+    user_name: str = Default("")
+    age: int = Default(0)
+
+
+class Sample:
+    id: int
+    user_name: str
+    age: int
+
+    def __init__(self, id: int, user_name: str, age: int):
+        self.id = id
+        self.user_name = user_name
+        self.age = age
+
+
+app = FastAPI()
+
+sample_list = []
+
+
+@app.get("/sample")
+async def get_all_sample():
+    return JSONResponse(
+        status_code=200,
+        content={"sample_list": sample_list, "sample_size": sample_list.__len__()},
+    )
+
+
+@app.post("/sample")
+async def add_sample(request_sample: RequestSample):
+    new_sample = Sample(
+        sample_list.__len__() + 1, request_sample.user_name, request_sample.age
+    )
+
+    sample_list.append(new_sample)
+
+    return JSONResponse(
+        status_code=200,
+        content={"sample_position": sample_list.__len__() - 1},
+    )
+
+
+@app.get("/sample/{id}")
+async def get_sample(id: int):
+    sample = None
+    for s in sample_list:
+        if s.id == id:
+            sample = s
+            return JSONResponse(
+                status_code=200,
+                content={"sample": sample},
+            )
+
+    raise HTTPException(status_code=404, detail="Sample not found")
+
+
+@app.delete("/sample/{id}")
+async def delete_sample(id: int):
+    for s in sample_list:
+        if s.id == id:
+            sample_list.remove(s)
+            return JSONResponse(status_code=200, content={"message": "Sample deleted"})
+
+    raise HTTPException(status_code=404, detail="Sample not found")
+
+
+@app.put("/sample/{id}")
+async def update_sample(id: int, q: str = None):
+    old_sample = [copy.deepcopy(s) for s in sample_list if s.id == id]
+
+    change_field = [pair.split("=") for pair in q.split("&")]
+
+    for s in sample_list:
+        if s.id == id:
+            for field in change_field:
+                if field[0] == "user_name":
+                    s.user_name = field[1]
+                elif field[0] == "age":
+                    s.age = int(field[1])
+                else:
+                    raise HTTPException(status_code=400, detail="Invalid field")
+
+    new_sample = [copy.deepcopy(s) for s in sample_list if s.id == id]
+
+    return JSONResponse(
+        status_code=200, content={"old_sample": old_sample, "new_sample": new_sample}
+    )
